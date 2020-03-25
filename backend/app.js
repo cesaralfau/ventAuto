5495bd822745840b400a1a9880d60d43eac53544
victor <vjose1903@outlook.es>
2020-03-24 22:08:36 -0400

    agregar gitignore

--- backend/app.js
+++ backend/app.js
@@ -12,29 +12,19 @@ const path = require("path");
 //rutas
 const rtsIndex = require("./routes/usuarios.routes");
 const rtsPhotos = require("./routes/photos.routes");
-const rtsCentros = require("./routes/centros.routes");
-const rtsSupli = require("./routes/suplidores.routes");
-const rtsTipoCentros = require("./routes/tipoCentros.routes");
-const rtsofertas = require("./routes/ofertas.routes");
-const rtsreservas = require("./routes/reservas.routes");
-const rtsreservas_fav = require("./routes/reservas_fav.routes");
+
 
 var app = express();
 
 // middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(cors({ origin: "http://localhost:4200" }));
+app.use(cors({ origin: "http://localhost:5200" }));
 app.use(passport.initialize());
 
 app.use("/api/usuarios", rtsIndex);
 app.use("/api/photos", rtsPhotos);
-app.use("/api/centros", rtsCentros);
-app.use("/api/tipoCentros", rtsTipoCentros);
-app.use("/api/supli", rtsSupli);
-app.use("/api/ofertas", rtsofertas);
-app.use("/api/reservas", rtsreservas);
-app.use("/api/reservas_fav", rtsreservas_fav);
+
 
 // error handler
 app.use((err, req, res, next) => {
